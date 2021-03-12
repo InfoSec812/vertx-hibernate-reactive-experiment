7aeabe6d462a9189368c5ce794221d4ad6c80ba7
Deven Phillips <deven.phillips@redhat.com>
2021-03-11 23:03:03 -0500

    Reduced persistence.xml

--- src/main/java/com/zanclus/vertx/MainVerticle.java
+++ src/main/java/com/zanclus/vertx/MainVerticle.java
@@ -3,29 +3,42 @@ package com.zanclus.vertx;
 import com.zanclus.vertx.models.Todo;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
-import org.hibernate.reactive.stage.Stage;
+import org.hibernate.reactive.mutiny.Mutiny;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainVerticle extends AbstractVerticle {
 
     EntityManagerFactory emf;
-    private Stage.SessionFactory sessionFactory;
+    private Mutiny.SessionFactory sessionFactory;
 
     @Override
     public void start(Promise<Void> start) {
+        Map<String, String> dbSettings = new HashMap<>();
+        dbSettings.put("javax.persistence.jdbc.url","jdbc:postgresql://localhost:5432/todo");
+        dbSettings.put("javax.persistence.jdbc.user","todo");
+        dbSettings.put("javax.persistence.jdbc.password","todo");
+        dbSettings.put("hibernate.connection.pool_size","10");
+        dbSettings.put("javax.persistence.schema-generation.database.action","drop-and-create");
+        dbSettings.put("javax.persistence.create-database-schemas","true");
+        dbSettings.put("hibernate.show_sql","true");
+        dbSettings.put("hibernate.format_sql","true");
+        dbSettings.put("hibernate.highlight_sql","true");
+
         try {
-            emf = Persistence.createEntityManagerFactory("postgresql");
+            emf = Persistence.createEntityManagerFactory("postgresql", dbSettings);
         } catch (Throwable e) {
             e.printStackTrace();
         }
 
         try {
-            sessionFactory = emf.unwrap(Stage.SessionFactory.class);
+            sessionFactory = emf.unwrap(Mutiny.SessionFactory.class);
         } catch (Throwable e) {
             e.printStackTrace();
         }
@@ -42,16 +55,22 @@ public class MainVerticle extends AbstractVerticle {
         todo.setCreated(LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()));
         sessionFactory
             .withTransaction((session, tx) -> session.persist(todo))
-            .exceptionally(this::logError)
-            .whenComplete(this::logAction);
+            .onFailure(this::logError)
+                .recoverWithNull()
+            .subscribe()
+                .with(this::logAction);
     }
 
-    private void logAction(Void unused, Throwable throwable) {
-        System.out.println("Added Todo");
+    private void logAction(Void unused) {
+        System.out.println("Persisted Todo item");
     }
 
-    private Void logError(Throwable throwable) {
+    private void logAction() {
+        System.out.println("Persisted Todo item");
+    }
+
+    private boolean logError(Throwable throwable) {
         throwable.printStackTrace();
-        return null;
+        return false;
     }
 }
